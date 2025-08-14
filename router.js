8ad5d7f9293b9eb722a07540f590af902c32af72
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-14 19:50:38 +0300

    Update router.js
    import podcastController
    mount podcastController middleware on the router

--- router.js
+++ router.js
@@ -2,10 +2,12 @@
 import { Router } from 'express'
 import postController from './src/controllers/postController.js'
 import authController from './src/controllers/authController.js'
+import podcastController from './src/controllers/podcastController.js'
 
 const routes = Router()
 
 routes.use(postController)
+routes.use(podcastController)
 routes.use('/users', authController)
 
 

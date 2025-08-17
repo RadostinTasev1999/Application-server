c6f984646fb72a6d7b4b08bf4e0f76e76deadf84
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-17 18:14:32 +0300

    Update util.js

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,10 +2,12 @@ const getErrorMessage = (error) => {
     
     switch(error.name) {
         case 'ValidationError':
+            debugger
             console.log('Error from back-end is:', error.message)
             return Object.values(error.errros).at(0).message
 
         default:
+            console.log('Error from getErrorMessage is:', error.message)
             return 'Error from Back-End is:', error.message
     }
 }

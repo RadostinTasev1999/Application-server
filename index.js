b5a64972a4b248c281ddae90c13d84a648bc809d
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-12-21 12:47:08 +0200

    Update index.js
    update endpoint url's

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import cors from 'cors'
 
 const app = express();
 
-const url = 'mongodb://127.0.0.1:27017'
+const url = 'mongodb://127.0.0.1:27017/'
+//mongodb://localhost:27017/
 
 await mongoose.connect(url, { dbName: 'AngularProject'})
         .then(() => {
@@ -20,7 +21,7 @@ await mongoose.connect(url, { dbName: 'AngularProject'})
 // Allow requests from localhost:4200
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: `http://localhost:4200`,
     credentials: true // set true if using cookies or auth headers.
 }))
 
@@ -34,8 +35,8 @@ app.use(express.urlencoded({extended: false}))
 
 app.use(routes)
 
-const port = 3000;
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-    console.log(`App is running on port http://localhost:3000`)
+    console.log(`App is running on port http://localhost:${process.env.PORT || 3030}`)
 })
